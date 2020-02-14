6cff0822509739192bea97177ea98d18737b03f3
jerrypunzalan24 <jerrypunzalan24@gmail.com>
2020-02-14 12:04:04 +0800

    V0.15.2a - Redesign Dashboard Layout 1

--- resources/views/_navbar.blade.php
+++ resources/views/_navbar.blade.php
@@ -9,27 +9,34 @@
 </div>
 
 {{-- Mobile View --}}
-<div class="is-hidden-desktop has-text-centered">
-	<nav class="navbar is-fixed-top">
-		<div class="navbar-item">
-			<div class="content">
-				<h4 id="title">MAP</h4>
-			</div>
-		</div>
-	</nav>
-	<nav class="navbar is-fixed-bottom">
-		<div class="navbar-brand">
-			<a class="navbar-item">
-				<div id="profile" class="has-text-left"><i class="is-size-4 fas fa-user"></i></div>
-			</a>
-			<a class="navbar-item">
-				<div id="settings" class="has-text-right"><i class="is-size-4 fas fa-bars"></i></div>
-			</a>
-		</div>
-	</nav>
-	<figure id="center" class="image is-64x64">
-		<a>
-			<img class="is-rounded" src="{{ asset('img/CenterLogo.png') }}" alt="MAP PIN">
-		</a>
-	</figure>
+<div class="is-hidden-desktop">
+	<nav class="navbar is-fixed-top has-text-center">
+    <div class="navbar-brand">
+      <div class="navbar-item">
+        <div class="content">
+          <h4>MAP</h4>
+        </div>
+      </div>
+      <a class="navbar-burger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </a>
+    </div>
+  </nav>
+  <nav class="navbar is-fixed-bottom">
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <div id="profile" class="has-text-left"><i class="is-size-4 fas fa-user"></i></div>
+      </a>
+      <a class="navbar-item has-dropdown has-dropdown-up">
+        <div id="settings" class="has-text-right"><i class="is-size-4 fas fa-bars"></i></div>
+      </a>
+    </div>
+  </nav>
+  <figure id="center" class="image is-64x64">
+    <a>
+     <img class="is-rounded" src="{{ asset('img/CenterLogo.png') }}" alt="MAP PIN">
+   </a>
+ </figure>
 </div>
