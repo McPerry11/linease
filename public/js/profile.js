1b02af208e5ea28740afdd470329408a83a2a856
McPerry11 <mack.perry.co@gmail.com>
2020-10-22 11:18:45 +0800

    V0.42.4b - Modified username invalid message

--- public/js/profile.js
+++ public/js/profile.js
@@ -178,7 +178,7 @@ $(function() {
           }
         });
       } else {
-        $('#username-warning').text('Username must be between 5 to 30 characters with at least 1 alphabetical character');
+        $('#username-warning').text('Username must be between 5 to 20 characters with at least 1 alphabetical character');
         $(this).addClass('is-danger').removeAttr('readonly');
         $('#username').removeClass('is-loading');
       }
