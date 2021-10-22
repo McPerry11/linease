a219a9567cadb3079ce81873db02d18ace7bf46b
Mack Perry Co <mack.perry.co@gmail.com>
2021-10-22 18:17:28 +0800

    V0.49.3 - Added user id in reports table. Added default panelist accounts

--- database/migrations/2020_03_02_175743_create_reports_table.php
+++ database/migrations/2020_03_02_175743_create_reports_table.php
@@ -15,6 +15,7 @@ class CreateReportsTable extends Migration
     {
         Schema::create('reports', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
             $table->string('latitude');
             $table->string('longitude');
             $table->enum('severity', [
