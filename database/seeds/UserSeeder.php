bf0e12d789e5a37455dacd313e1c993ae64007ca
Mack Perry Co <mack.perry.co@gmail.com>
2021-11-10 16:04:41 +0800

    V0.52.16 - Added sample accounts in user seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,9 +13,9 @@ class UserSeeder extends Seeder
     public function run()
     {
         $data = [
-            'username' => ['McPerry_', 'seamonster618', 'jbrtfrnndz', 'youngG', 'kaysabelle', 'panelist1', 'panelist2', 'panelist3'],
-            'email' => ['mack.perry.co@gmail.com', 'acvtela@yahoo.com', 'jbrtfrnndz@gmail.com', 'guiang.justing@gmail.com', 'kaye.rosacia@gmail.com', 'panelist1@sample.com', 'panelist2@sample.com', 'panelist3@sample.com'],
-            'password' => ['237845691', 'cuteasianboi', 'hentaihart', 'Justin23', 'Awitser13', 'panelist1', 'panelist2', 'panelist3']
+            'username' => ['McPerry_', 'seamonster618', 'jbrtfrnndz', 'youngG', 'kaysabelle', 'panelist1', 'panelist2', 'panelist3', 'sample'],
+            'email' => ['mack.perry.co@gmail.com', 'acvtela@yahoo.com', 'jbrtfrnndz@gmail.com', 'guiang.justing@gmail.com', 'kaye.rosacia@gmail.com', 'panelist1@sample.com', 'panelist2@sample.com', 'panelist3@sample.com', 'sample@sample.com'],
+            'password' => ['237845691', 'cuteasianboi', 'hentaihart', 'Justin23', 'Awitser13', 'panelist1', 'panelist2', 'panelist3', 'samplepassword']
         ];
         $types = ['SUPER', 'ADMIN', 'FACIL', 'USER'];
 
